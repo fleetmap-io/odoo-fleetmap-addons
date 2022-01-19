38d6caf5528c26d4a4023f45f57d6e34e42f2fc8
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-01-19 10:32:43 +0000

    starting field-service fleetmap

---
+++ fieldservice_fleetmap/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Field Service - Fleetmap',
+    'version': '0.0.1',
+    'summary': 'Integration with fleetmap',
+    'category': 'Field Service',
+    'website': '',
+    'depends': ['fieldservice'],
+    'auto_install': True,
+    'license': 'LGPL-3'
+}
