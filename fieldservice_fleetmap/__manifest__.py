4045e356af8077b79370feefc4829f6744224855
jcardus <asklocation.net@gmail.com>
2022-01-24 17:17:10 +0000

    manifest

--- fieldservice_fleetmap/__manifest__.py
+++ fieldservice_fleetmap/__manifest__.py
@@ -1,12 +1,15 @@
-# Copyright (C) 2018 Open Source Integrators
+# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2019 Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     "name": "Field Service Fleetmap",
-    "summary": "Manage Field Service vehicles and assign drivers",
+    "summary": "Link Field Service vehicles with Odoo Fleet",
     "version": "13.0.1.0.0",
     "category": "Field Service",
-    "author": "Open Source Integrators, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/field-service",
+    "author": "fleetmap",
     "depends": ["fieldservice"],
-    "license": "AGPL-3"
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "installable": True,
 }
