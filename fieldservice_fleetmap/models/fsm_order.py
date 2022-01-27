1b1243c0138c458cc17b0e1d079e760b62689b6d
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-01-27 22:01:20 +0000

    starting firebase

--- fieldservice_fleetmap/models/fsm_order.py
+++ fieldservice_fleetmap/models/fsm_order.py
@@ -1,11 +1,16 @@
 # Copyright (C) 2018 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
 import logging
 import requests
 import os
+import firebase_admin
+from odoo import api, fields, models
 from urllib.parse import urljoin
+from firebase_admin import firestore
+
+default_app = firebase_admin.initialize_app()
+db = firestore.client()
 
 endpoint = os.environ.get('MIDDLEWARE_URL')
 
@@ -23,6 +28,8 @@ class FSMOrder(models.Model):
         _logger.info("vals: %s", vals)
         url = urljoin(endpoint, 'fsm/order/create')
         _logger.info('url: %s, result: %s', url, requests.post(url, vals))
+        doc_ref = db.collection(u'jobs').document(vals.name)
+        doc_ref.set(vals)
         return res
 
     @api.onchange("location_id")
