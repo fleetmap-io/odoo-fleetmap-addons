389702ce86cb574a350a3d0babf81d8fab61ad24
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-01-28 13:18:09 +0000

    remove post

--- fieldservice_fleetmap/models/fsm_order.py
+++ fieldservice_fleetmap/models/fsm_order.py
@@ -6,7 +6,6 @@ import requests
 import os
 import firebase_admin
 from odoo import api, fields, models
-from urllib.parse import urljoin
 from firebase_admin import firestore
 
 default_app = firebase_admin.initialize_app()
@@ -19,15 +18,10 @@ _logger = logging.getLogger(__name__)
 class FSMOrder(models.Model):
     _inherit = "fsm.order"
 
-    # Geometry Field
-    # shape = fields.GeoPoint("Coordinate")
-
     @api.model
     def create(self, vals):
         res = super().create(vals)
         _logger.info("vals: %s", vals)
-        url = urljoin(endpoint, 'fsm/order/create')
-        _logger.info('url: %s, result: %s', url, requests.post(url, vals))
         doc_ref = db.collection(u'jobs').document(vals['name'])
         keys_values = vals.items()
         doc_ref.set({str(key): str(value) for key, value in keys_values})
